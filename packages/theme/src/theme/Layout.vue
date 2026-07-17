a0cfabcdf736432d4832c4e35672e30edaa0ded6
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-07-18 02:26:04 +0800

    refactor(layout/home): 重命名布局组件
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- packages/theme/src/theme/Layout.vue
+++ packages/theme/src/theme/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <HomePage v-if="frontmatter.layout === 'home'" />
+  <HomeLayout v-if="frontmatter.layout === 'home'" />
   <Layout v-else />
 </template>
 
@@ -8,9 +8,9 @@ import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import type { Ref } from 'vue'
 
-import HomePage from '#/layouts/HomePage.vue'
+import HomeLayout from '#/layouts/HomeLayout.vue'
 import type { ThemeConfig } from '#/types'
-import type { Frontmatter } from '#/utils/frontmatter/'
+import type { Frontmatter } from '#/utils/frontmatter'
 
 const Layout = DefaultTheme.Layout
 const data = useData<ThemeConfig>()
--- packages/theme/src/theme/layouts/HomeLayout.vue
+++ packages/theme/src/theme/layouts/HomeLayout.vue
@@ -5,9 +5,12 @@
 <script lang="ts" setup>
 import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import type { Ref } from 'vue'
 
 import type { ThemeConfig } from '#/types'
+import type { HomeFrontmatter } from '#/utils/frontmatter'
 
 const Layout = DefaultTheme.Layout
-const { frontmatter } = useData<ThemeConfig>()
+const data = useData<ThemeConfig>()
+const frontmatter = data.frontmatter as Ref<HomeFrontmatter>
 </script>
